0ddf08720b9ff2492fdd75a18e62b5f3e2bc5196
kgreav <kurt201@hotmail.com>
2013-10-13 17:56:45 +1100

    Mock callback implementation

---
+++ src/java/test/com/redhat/victims/plugin/eclipse/mockCallbackHandler.java
@@ -0,0 +1,14 @@
+package com.redhat.victims.plugin.eclipse;
+
+import com.redhat.victims.VictimsException;
+import com.redhat.victims.plugin.eclipse.handler.SettingsCallback;
+
+public class mockCallbackHandler implements SettingsCallback {
+
+	@Override
+	public int callbackSettings() throws VictimsException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
